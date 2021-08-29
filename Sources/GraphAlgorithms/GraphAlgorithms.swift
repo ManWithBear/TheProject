8e61ccd7348564a63d4d43dd3ae8e57c97543d1f
Denis Bogomolov <denis@manwithbear.com>
2021-08-29 03:23:14 +0200

    Implement topological sorting

--- Sources/GraphAlgorithms/GraphAlgorithms.swift
+++ Sources/GraphAlgorithms/GraphAlgorithms.swift
@@ -1,3 +1,45 @@
-struct GraphAlgorithms {
-    var text = "Hello, World!"
+public class TopologicalIterator<Vertex>: IteratorProtocol {
+    public func next() -> Vertex? {
+        nil
+    }
+}
+
+public struct Graph<Vertex: Hashable> {
+    public var vertices: [Vertex]
+    public var edges: [Vertex: [Vertex]]
+    
+    public init(vertices: [Vertex], edges: [Vertex: [Vertex]]) {
+        self.vertices = vertices
+        self.edges = edges
+    }
+    
+    /// Sort `vertices` in a topological order.
+    /// Using simplified DFS algorithm (assume graph is DAG)
+    /// https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+    public mutating func sort() {
+        var topologicalVertices: [Vertex] = []
+        var notVisitedVertices: Set<Vertex> = Set(vertices)
+        var stack: [Vertex] = []
+        
+        while let entryNode = notVisitedVertices.popFirst() {
+            stack.append(entryNode)
+            while let vertex = stack.last {
+                defer {
+                    if stack.last == vertex {
+                        stack.removeLast()
+                        topologicalVertices.insert(vertex, at: 0)
+                    }   
+                }
+                
+                guard let edges = self.edges[vertex] else { continue }
+                
+                for neighbor in edges where notVisitedVertices.contains(neighbor) {
+                    notVisitedVertices.remove(neighbor)
+                    stack.append(neighbor)
+                }
+            }
+        }
+        
+        self.vertices = topologicalVertices
+    }
 }
--- Tests/GraphAlgorithmsTests/GraphAlgorithmsTests.swift
+++ Tests/GraphAlgorithmsTests/GraphAlgorithmsTests.swift
@@ -1,11 +1,66 @@
-    import XCTest
-    @testable import GraphAlgorithms
+import XCTest
+import GraphAlgorithms
 
-    final class GraphAlgorithmsTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(GraphAlgorithms().text, "Hello, World!")
+final class GraphAlgorithmsTests: XCTestCase {
+    func testTopologicalSort1() {
+        var graph = Graph(
+            vertices: [2, 3, 5, 7, 8, 9, 10, 11], 
+            edges: [
+                5: [11],
+                7: [11, 8],
+                3: [8, 10],
+                11: [2, 9, 10],
+                8: [9]
+            ]
+        )
+        
+        graph.sort()
+
+        assertInTopologicalOrder(graph)
+    }
+
+    func testTopologicalSort2() {
+        var graph = Graph(
+            vertices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+            edges: [
+                1: [2, 3],
+                2: [4, 5, 6],
+                3: [7, 8, 9],
+                4: [10],
+                5: [10],
+                6: [10],
+                7: [10],
+                8: [10],
+                9: [10],
+                10: [11]
+            ]
+        )
+
+        graph.sort()
+
+        assertInTopologicalOrder(graph)
+    }
+}
+
+extension XCTestCase {
+
+    /// Graph can have multiple valid topological orders.
+    /// Graph in a topological order if and only if for every directed edge uv from vertex u to vertex v, u comes before v in the ordering.
+    func assertInTopologicalOrder<T: Hashable>(_ graph: Graph<T>, file: StaticString = #file, line: UInt = #line) {
+        for (vertex, edges) in graph.edges {
+            guard let vIndex = graph.vertices.firstIndex(of: vertex) else {
+                XCTFail("Missing vertex after sort - \(vertex)", file: file, line: line)
+                if continueAfterFailure { continue }
+                break
+            }
+            for edge in edges {
+                guard let eIndex = graph.vertices.firstIndex(of: edge) else {
+                    XCTFail("Missing vertex after sort - \(vertex)", file: file, line: line)
+                    if continueAfterFailure { continue }
+                    break
+                }
+                XCTAssert(vIndex < eIndex, "Wrong order of \(vertex) - \(edge), got \(vIndex) and \(eIndex)", file: file, line: line)
+            }
         }
     }
+}
