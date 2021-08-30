4d05388c5cee4e66bff076ed55b9beb9320ccf89
Denis Bogomolov <denis@manwithbear.com>
2021-08-31 01:16:00 +0200

    Simplify Graph lib

--- Sources/GraphAlgorithms/GraphAlgorithms.swift
+++ Sources/GraphAlgorithms/GraphAlgorithms.swift
@@ -1,7 +1,34 @@
-public class TopologicalIterator<Vertex>: IteratorProtocol {
-    public func next() -> Vertex? {
-        nil
+/// Sort graph vertices in a topological order assuming that graph is DAG.
+/// Using simplified DFS algorithm.
+/// https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+public func sortInTopologicalOrder<Vertex: Hashable, Edges: Sequence>(
+    vertices: [Vertex],
+    edgesProvider: (Vertex) -> Edges?
+) -> [Vertex] where Edges.Element == Vertex {
+    var topologicalVertices: [Vertex] = []
+    var notVisitedVertices: Set<Vertex> = Set(vertices)
+    var stack: [Vertex] = []
+
+    while let entryNode = notVisitedVertices.popFirst() {
+        stack.append(entryNode)
+        while let vertex = stack.last {
+            defer {
+                if stack.last == vertex {
+                    stack.removeLast()
+                    topologicalVertices.insert(vertex, at: 0)
+                }
+            }
+
+            guard let edges = edgesProvider(vertex) else { continue }
+
+            for neighbor in edges where notVisitedVertices.contains(neighbor) {
+                notVisitedVertices.remove(neighbor)
+                stack.append(neighbor)
+            }
+        }
     }
+
+    return topologicalVertices
 }
 
 public struct Graph<Vertex: Hashable> {
@@ -17,29 +44,6 @@ public struct Graph<Vertex: Hashable> {
     /// Using simplified DFS algorithm (assume graph is DAG)
     /// https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
     public mutating func sort() {
-        var topologicalVertices: [Vertex] = []
-        var notVisitedVertices: Set<Vertex> = Set(vertices)
-        var stack: [Vertex] = []
-        
-        while let entryNode = notVisitedVertices.popFirst() {
-            stack.append(entryNode)
-            while let vertex = stack.last {
-                defer {
-                    if stack.last == vertex {
-                        stack.removeLast()
-                        topologicalVertices.insert(vertex, at: 0)
-                    }   
-                }
-                
-                guard let edges = self.edges[vertex] else { continue }
-                
-                for neighbor in edges where notVisitedVertices.contains(neighbor) {
-                    notVisitedVertices.remove(neighbor)
-                    stack.append(neighbor)
-                }
-            }
-        }
-        
-        self.vertices = topologicalVertices
+        self.vertices = sortInTopologicalOrder(vertices: vertices, edgesProvider: { edges[$0] })
     }
 }
